fde22d0ab1c3a8f8fa8611c0c6a112f899a97c71
Abdi Yusuf <abdiyuu7@gmail.com>
2021-06-09 22:33:11 -0600

    added visually appealing UI elements, added button and input for canvas size changing

--- app.js
+++ app.js
@@ -1,23 +1,39 @@
-function askSize() {
-	let size = 0
-	do {
-		size = prompt('Enter a size between 1 and 64')
-	} while (size < 1 || size > 64)
-	return size;
-}
-
 function makeGrid(sq) {
 	const grid = document.querySelector('.container')
 	grid.style.display='grid';
-	grid.style.height='100vh';
 	grid.style.gridTemplateColumns=`repeat(${sq}, ${100/sq}%)`;
 	grid.style.gridTemplateRows=`repeat(${sq}, ${100/sq}%)`;
 	for(let i = 0; i < sq*sq; i++) {
 		const box = document.createElement('div');
 		box.style.border='1px solid black'
+		box.classList.add('square')
 		box.innerText='\n'
 		grid.appendChild(box);
 	}
 }
 
-makeGrid(askSize())
+function changeGrid() {
+	const input = document.querySelector('#size')
+	size = Number(input.value)
+	return (size < 1 || size > 64 || !size) ? 16 : size;
+}
+
+
+function fillGrid(e) {
+        e.target.style.background = 'black'
+}
+
+
+const boxes = document.querySelectorAll('.square')
+const grid = document.querySelector('.container')
+const confirm_button = document.querySelector('#confirm')
+boxes.forEach(box => box.addEventListener('mouseover', fillGrid));
+grid.addEventListener('touchmove', fillGrid);
+confirm_button.addEventListener('click', makeGrid(changeGrid))
+
+makeGrid(16)
+
+
+const input = document.querySelector('#size')
+input.addEventListener('focus',(e) => e.target.removeAttribute('placeholder'))
+input.addEventListener('blur', (e) => e.target.setAttribute('placeholder', '1-16'))
